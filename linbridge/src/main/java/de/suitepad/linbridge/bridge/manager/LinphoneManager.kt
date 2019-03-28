c56dc6a3c5d469878df65de2590709d658f4b811
Ahmed Mkaouar <ahmed.mkaouar@suitepad.de>
2019-03-28 16:34:56 +0100

    CHORE: implement keepalive timer in LinphoneManager

--- linbridge/src/main/java/de/suitepad/linbridge/bridge/manager/LinphoneManager.kt
+++ linbridge/src/main/java/de/suitepad/linbridge/bridge/manager/LinphoneManager.kt
@@ -2,13 +2,23 @@ package de.suitepad.linbridge.bridge.manager
 
 import de.suitepad.linbridge.api.SIPConfiguration
 import org.linphone.core.Core
+import java.util.*
 
 class LinphoneManager(val core: Core): IManager {
 
+    val keepAliveTask = object : TimerTask() {
+        override fun run() {
+            core.iterate()
+        }
+    }
+    val keepAliveTimer = Timer("Linphone scheduler")
+
     override fun start() {
+        keepAliveTimer.schedule(keepAliveTask, 0, 20)
     }
 
     override fun destroy() {
+        keepAliveTimer.cancel()
     }
 
     override fun configure(settings: SIPConfiguration?) {
